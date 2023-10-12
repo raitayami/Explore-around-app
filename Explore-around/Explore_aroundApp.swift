28dc9b9726063d8e617b3bdd00957f408cedc67a
tayamirai9 <128257364+tayamirai9@users.noreply.github.com>
2023-10-12 13:13:29 +0100

    Initial Commit

---
+++ Explore-around/Explore_aroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Explore_aroundApp.swift
+//  Explore-around
+//
+//  Created by Tayami Rai on 12/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Explore_aroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
